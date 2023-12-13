df719dc78e5d80cd1e318c12aa3e6f6ce23748df
syahirarr <152838355+syahirarr@users.noreply.github.com>
2023-12-13 23:17:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleVideo(){
+    const trailer = document.querySelector('.trailer');
+    const video = document.querySelector('video');
+    video.pause();
+    trailer.classList.toggle('active');
+}
+
